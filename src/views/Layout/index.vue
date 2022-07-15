5ed37c03cdad3aae94ba27d3cf78839e41b4f8dc
newSmile726 <1432072169@qq.com>
2022-07-15 17:53:00 +0800

    完成布局页面路由配置

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <!-- 二级路由出口 -->
+    <router-view></router-view>
+    <van-tabbar class="footer-text" route>
+        <van-tabbar-item to='/ '>
+            <template #icon>
+                <i class="toutiao toutiao-shouye"></i>
+            </template>
+            首页
+        </van-tabbar-item>
+        <van-tabbar-item to='/video'>
+            <template #icon>
+                <i class="toutiao toutiao-shipin"></i>
+            </template>
+                视频
+        </van-tabbar-item>
+        <van-tabbar-item to='/qa'>
+            <template #icon>
+                <i class="toutiao toutiao-wenda"></i>
+            </template>
+                问答
+        </van-tabbar-item>
+        <van-tabbar-item to='/profile'>
+            <template #icon>
+                <i class="toutiao toutiao-wode"></i>
+            </template>
+                我的
+        </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+    }
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+<style scoped lang="less">
+.footer-text{
+    .toutiao{
+        font-size: 20px;
+    }
+}
+</style>
---
+++ src/views/Qa/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+<h1>资讯</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+
+    }
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+<style scoped>
+
+</style>
